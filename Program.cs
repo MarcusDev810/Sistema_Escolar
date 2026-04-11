d85f3a185f9dc53b728752691e8d60bc7143167b
Marcus Vinícius <marcusvini810.dev@gmail.com>
2026-04-10 22:19:45 -0300

    Criação da classe Aluno

--- Program.cs
+++ Program.cs
@@ -7,3 +7,18 @@ class Program
         Console.WriteLine("Hello, World!");
     }
 }
+
+public class Aluno{
+
+    public int Codigo{ get; private set;}
+    public string Nome{ get; private set; }
+    public double Nota{get; private set; }
+
+    public Alunos(int codigo, string nome, double nota ){
+        this.Codigo = codigo;
+        this.Nome = nome;
+        this.Nota = nota;
+    }
+
+}
+
